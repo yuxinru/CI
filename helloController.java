8f95ef1fbb05cec990d8596d1abfa60db5e63fc0
yuxinru <yxr164115@sjtu.edu.cn>
2018-05-22 15:58:16 +0800

    no message

---
+++ helloController.java
@@ -0,0 +1,8 @@
+package com.example.homework_3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class helloController {
+}
